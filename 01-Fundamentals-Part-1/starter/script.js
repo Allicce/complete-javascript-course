11ef6c53c7b6943584ae46ae43ed30f58dfe9b35
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-15 10:09:35 +0100

    variables and values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,18 @@
-console.log( 40 + 8);
+// console.log( 40 + 8);
+
+/////////////////////////////////////////
+//VALUES AND VARIABLES
+let firstName = 'Jonas';
+console.log(firstName);
+
+// illegal variables name:
+// let 3years = 3; variable can't start with number
+// let jonas&matilda = "JM" & is not allowed
+// let new = "new" new is reserved word
+
+let $function = 27;
+
+// constant write with uppercase
+const PI = 3.1415;
+
+

61ca264685d653c303ce17e197af6270dc4c03f3
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-17 10:20:37 +0100

    let, const, var + operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -34,3 +34,49 @@ console.log(typeof year);
 //typeof null return object
 console.log(typeof null);
 
+/////////////////////////////////////////
+//let const var
+
+//block scope
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+//must by declared with value
+// const job;
+
+//function scope
+// var job = 'programmer';
+// job = 'teacher';
+
+/////////////////////////////////////////
+//operators
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(2 ** 3);
+// 2 ** 3 means 2 to the power of free = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Davis';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+ x += 10;
+ console.log(x);
+ x++;
+ console.log(x);
+ x--;
+
+ //comparison operator
+console.log( ageJonas > ageSarah);
+
+/////////////////////////////////////////
+//operator precedence
+
+

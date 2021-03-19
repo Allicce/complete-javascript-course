4926c86c5fd4ed6f104c82e182660d58485afb98
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-19 16:42:23 +0100

    functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,28 @@
+'use strict';
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if(passTest) hasDriversLicense = true;
+//
+// if(hasDriversLicense) console.log('I can drive');
+
+////////////////////
+// functions
+
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+//
+// //calling / running / invoking function
+// logger();
+//
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} and ${oranges} oranges.`
+//     return juice;
+// }
+//
+// console.log(fruitProcessor(5, 0));
+//
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);

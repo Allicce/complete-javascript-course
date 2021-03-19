8b94ba40afd48ad7684a43f284743787959c812b
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-19 13:24:06 +0100

    truthy and falsy value

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -170,30 +170,60 @@ GOOD LUCK 😀
 //type conversion and coercion
 
 //type conversion
-const inputYear = '1918'
-console.log(inputYear + 18);
-console.log(Number(inputYear) + 18);
-console.log(String(23));
+// const inputYear = '1918'
+// console.log(inputYear + 18);
+// console.log(Number(inputYear) + 18);
+// console.log(String(23));
+//
+// //type coercion
+//
+// console.log('I am ' + 23 + 'years old');
+//
+// //convert to number
+// console.log('23' - '10' - 3);
+// //convert to string
+// console.log('23' + '10' + 3);
+// //convert to number
+// console.log('23' /'10');
+// console.log('23' * '10');
+//
+// let n = '1' + 1;
+// n = n - 1
+// console.log(n)
+//
+// console.log('23' - '10' - '3' - 2 + '5');
 
-//type coercion
+/////////////////////////////////////////
+//Truthy and Falsy values
 
-console.log('I am ' + 23 + 'years old');
-
-//convert to number
-console.log('23' - '10' - 3);
-//convert to string
-console.log('23' + '10' + 3);
-//convert to number
-console.log('23' /'10');
-console.log('23' * '10');
-
-let n = '1' + 1;
-n = n - 1
-console.log(n)
-
-console.log('23' - '10' - '3' - 2 + '5');
-
-
+// console.log(Boolean('Jonas'))
+// console.log(Boolean(undefined))
+// console.log(Boolean(0))
+// console.log(Boolean({}))
+// console.log(Boolean(''))
+//
+// const money = 0;
+//
+// if(money) {
+//     console.log("Don't spend it all!");
+// } else {
+//     console.log("You should get a job");
+// }
+//
+// let height;
+// if(height) {
+//     console.log("YaY height is defined")
+// } else {
+//     console.log('Height is undefined')
+// }
+//
+// //this code return else because 0 is false value
+// let height = 0;
+// if(height) {
+//     console.log("YaY height is defined")
+// } else {
+//     console.log('Height is undefined')
+// }
 
 
 

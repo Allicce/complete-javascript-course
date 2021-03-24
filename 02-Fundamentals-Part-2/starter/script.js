a10c796b02534021af62477245b7641cf4c811b3
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-24 21:49:44 +0100

    loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -236,6 +236,79 @@ GOOD LUCK 😀
 // mark.calcBMI()
 // console.log(mark.bmi)
 
+////////////////////
+// loop
+
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log('Lifting weights repetition ', rep);
+// }
+
+////////////////////
+// looping arrays breaking and continuing
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// }
+//
+// const types = []
+//
+// for(let i = 0; i < jonas.length; i++) {
+//     console.log(jonas[i], typeof jonas[i])
+//     // types[i] = typeof jonas[i]
+//     types.push(typeof jonas[i])
+// }
+// console.log(types)
+
+// const years = [1991, 2007, 1969, 2020]
+// const ages = []
+//
+// for(let i = 0; i < years.length; i++) {
+//     ages.push(2037 - years[i])
+// }
+//
+// console.log(ages)
+
+// continue and break
+// console.log('--- ONLY STRINGS ---')
+// for (let i = 0; i < jonas.length; i++) {
+//     if (typeof jonas[i] !== 'string') continue;
+//
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+//
+// console.log('--- BREAK WITH NUMBER ---')
+// for (let i = 0; i < jonas.length; i++) {
+//     if (typeof jonas[i] === 'number') break;
+//
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+
+////////////////////
+// while loop
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
+//
+// let rep = 1;
+// while (rep <= 10) {
+//     // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+//     rep++;
+// }
+//
+// let dice = Math.trunc(Math.random() * 6) + 1;
+//
+// while (dice !== 6) {
+//     console.log(`You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) console.log('Loop is about to end...');
+// }
+
+
 
 
 

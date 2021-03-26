abdf2e5b939cec1ebf7200f516504d0c83b08b39
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-26 22:07:40 +0100

    manipulating css style

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,6 +19,8 @@ document.querySelector('.check').addEventListener('click', () => {
         document.querySelector('.message').textContent = 'No number'
     } else if (secretNumber === guess) {
         document.querySelector('.message').textContent = 'Correct number'
+        document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem'
     } else if (guess > secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too high!'
@@ -28,7 +30,6 @@ document.querySelector('.check').addEventListener('click', () => {
             document.querySelector('.message').textContent = 'You lost the game!'
             document.querySelector('.score').textContent = 0;
         }
-
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too low!'
@@ -39,6 +40,4 @@ document.querySelector('.check').addEventListener('click', () => {
             document.querySelector('.score').textContent = 0;
         }
     }
-
-
 })

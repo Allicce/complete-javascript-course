64178f480e012d418bda6ecbbaa1f55970a1ff25
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-03-26 20:46:01 +0100

    click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,17 @@
 'use strict';
-console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent = 'Correct number!'
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent = 'Correct number!'
+//
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+//
+// console.log(document.querySelector('.guess').value)
+// document.querySelector('.guess').value = 23;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+document.querySelector('.check').addEventListener('click', () => {
+    const guess =Number(document.querySelector('.guess').value)
 
-console.log(document.querySelector('.guess').value)
-document.querySelector('.guess').value = 23;
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number'
+    }
+})

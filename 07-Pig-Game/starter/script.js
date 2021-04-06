1ba95cfe89ddc00c282b7094c7c63f9ad47e530e
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-04-06 17:22:01 +0200

    switching the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,14 +9,17 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0EL = document.getElementById('current--0');
 const current1EL = document.getElementById('current--1');
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 
 
 //starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
+const scores = [0,0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -31,11 +34,18 @@ btnRoll.addEventListener('click', () => {
     if(dice !== 1) {
         //add current score
         currentScore += dice;
-        current0EL.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // current0EL.textContent = currentScore;
 
 
     } else {
         //switch new player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0EL.classList.toggle('player--active');
+        player1EL.classList.toggle('player--active');
+
     }
 })
 

464c87907af6bb69879691380ee3541123125b1a
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-04-06 17:50:37 +0200

    change winner score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -77,7 +77,7 @@ btnHold.addEventListener('click', () => {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         //check if the score >=100
-        if(scores[activePlayer] >= 20) {
+        if(scores[activePlayer] >= 100) {
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');

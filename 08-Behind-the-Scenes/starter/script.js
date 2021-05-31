0138db7a5202621ffd1d8afe6097269d422c12f1
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-05-31 16:48:03 +0200

    this keyword in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -41,38 +41,69 @@ calcAge(1991);
 // console.log(job);
 // console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991;
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+//
+// console.log(addDec);
+// console.log(addExpr);
+// console.log(addArrow);
+//
+// function addDec(a,b) {
+//     return a + b;
+// }
+//
+// const addExpr = function(a,b) {
+//     return a + b;
+// }
+//
+// const addArrow = (a,b) => {
+//     return a + b;
+// }
+//
+// //Example
+// if(!numProducts) deleteShoppingCart()
+// var numProducts = 10;
+//
+// function deleteShoppingCart() {
+//     console.log('all products deleted');
+// }
+//
+// var x = 1;
+// let y = 2;
+// const z = 3;
+//
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
 
-console.log(addDec);
-console.log(addExpr);
-console.log(addArrow);
+// const calcAge = function (birthYear) {
+//     console.log(2037 - birthYear)
+//     console.log(this)
+// }
+// calcAge(1991);
+//
+// const calcAge2 = (birthYear) => {
+//     console.log(2037 - birthYear)
+//     console.log(this)
+// }
+// calcAge2(1991);
 
-function addDec(a,b) {
-    return a + b;
+const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(this)
+        console.log(2037 - this.year)
+    }
 }
+jonas.calcAge();
 
-const addExpr = function(a,b) {
-    return a + b;
+const matilda = {
+    year: 2017
 }
+//method borrowing
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
 
-const addArrow = (a,b) => {
-    return a + b;
-}
-
-//Example
-if(!numProducts) deleteShoppingCart()
-var numProducts = 10;
-
-function deleteShoppingCart() {
-    console.log('all products deleted');
-}
-
-var x = 1;
-let y = 2;
-const z = 3;
-
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+const f = jonas.calcAge;
+f();

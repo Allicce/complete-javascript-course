4828f45a0641edacab5432b683d458c10902ab73
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-03 20:29:32 +0200

    challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -173,44 +173,132 @@ restaurant.orderDelivery({
 
 //use ANY data type, return ANY data type, short-circuiting
 //results is 3
-console.log(3 || 'Jonas');
-//results Jonas
-console.log('' || 'Jonas');
-//return true
-console.log(true || 0);
-//return null
-console.log(undefined || null);
-// return Hello
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// console.log(3 || 'Jonas');
+// //results Jonas
+// console.log('' || 'Jonas');
+// //return true
+// console.log(true || 0);
+// //return null
+// console.log(undefined || null);
+// // return Hello
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+//
+// const guest1 = restaurant.numGuests ? restaurant.numGuests: 10;
+//
+// console.log(guest1);
+//
+// const guest2 = restaurant.numGuests || 10;
+//
+// console.log('------------AND----------');
+// //return 0
+// console.log(0 && 'Jonas');
+// //return Jonas
+// console.log(7 && 'Jonas');
+// //return null
+// console.log('Hello' && 23 && null && 'Jonas')
+//
+// if(restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+//
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+//
+// //Nullish coalescing operator
+// restaurant.numGuests = 0;
+// const guest = restaurant.numGuests || 10;
+// console.log(guest);
+//
+// //Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
-const guest1 = restaurant.numGuests ? restaurant.numGuests: 10;
 
-console.log(guest1);
+///////////////////////////////////////
+// Coding Challenge #1
 
-const guest2 = restaurant.numGuests || 10;
+/*
+We're building a football betting app (soccer for my American friends 😅)!
 
-console.log('------------AND----------');
-//return 0
-console.log(0 && 'Jonas');
-//return Jonas
-console.log(7 && 'Jonas');
-//return null
-console.log('Hello' && 23 && null && 'Jonas')
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-if(restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//1
+const [players1, players2] = game.players;
+console.log(players1, players2);
+//2
+const [goalKeeper, ...fieldPlayers] = players1;
+console.log(goalKeeper, fieldPlayers);
+//3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+//4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+//5
+const {odds: {team1, x: draw, team2}} = game
+console.log(team1, draw, team2);
+//6
+const printGoals = function(...players) {
+ console.log(`${players.length} goals were scored`)
 }
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
-//Nullish coalescing operator
-restaurant.numGuests = 0;
-const guest = restaurant.numGuests || 10;
-console.log(guest);
-
-//Nullish: null and undefined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+//7
+team1 < team2 && console.log('team 1 is more likely to win');
+team1 > team2 && console.log('team 2 is more likely to win');
 
 
 

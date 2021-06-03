4f7988bde075aadae7fe08e832a9d4a526b56a4f
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-03 20:37:17 +0200

    for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -274,32 +274,46 @@ const game = {
     team2: 6.5,
   },
 };
-//1
-const [players1, players2] = game.players;
-console.log(players1, players2);
-//2
-const [goalKeeper, ...fieldPlayers] = players1;
-console.log(goalKeeper, fieldPlayers);
-//3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-//4
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
-//5
-const {odds: {team1, x: draw, team2}} = game
-console.log(team1, draw, team2);
-//6
-const printGoals = function(...players) {
- console.log(`${players.length} goals were scored`)
-}
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals(...game.scored);
+// //1
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+// //2
+// const [goalKeeper, ...fieldPlayers] = players1;
+// console.log(goalKeeper, fieldPlayers);
+// //3
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+// //4
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+// //5
+// const {odds: {team1, x: draw, team2}} = game
+// console.log(team1, draw, team2);
+// //6
+// const printGoals = function(...players) {
+//  console.log(`${players.length} goals were scored`)
+// }
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
+//
+// //7
+// team1 < team2 && console.log('team 1 is more likely to win');
+// team1 > team2 && console.log('team 2 is more likely to win');
 
-//7
-team1 < team2 && console.log('team 1 is more likely to win');
-team1 > team2 && console.log('team 2 is more likely to win');
+const menu2 = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
+for(const item of menu2) console.log(item);
+//index
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`)
+};
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`)
+};
+
+//array iterators
+console.log(menu.entries());
 
 
 

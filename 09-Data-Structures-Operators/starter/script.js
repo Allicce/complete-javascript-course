cf1e3f5574c065a330b3d7d5833298b0c05074e1
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-22 10:56:03 +0200

    maps iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -440,42 +440,73 @@ GOOD LUCK 😀
 // //clear all elements
 // ordersSet.clear()
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano')
-rest.set(1, 'Firenze, Italy')
-rest.set(2, 'Lisbon Portugal')
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano')
+// rest.set(1, 'Firenze, Italy')
+// rest.set(2, 'Lisbon Portugal')
+//
+// rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//     .set('open', 11)
+//     .set('close', 23)
+//     .set(true, 'we are open')
+//     .set(false, 'we are closed')
+//
+// console.log(rest.get('name'))
+// console.log(rest.get(true))
+//
+// const time = 21;
+//
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+//
+// console.log(rest.has('categories'))
+// rest.delete('2')
+// console.log(rest)
+//
+// console.log(rest.size)
+//
+// rest.set([1,2], 'Test')
+// console.log(rest)
+//
+// //not the same object as a key on rest
+// console.log(rest.get([1,2]))
+//
+// //fix
+// const arr = [1,2]
+// rest.set(arr, 'Test')
+// console.log(rest.get(arr))
+//
+// rest.set(document.querySelector('h1'), 'Heading')
+//
+// rest.clear()
 
-rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-    .set('open', 11)
-    .set('close', 23)
-    .set(true, 'we are open')
-    .set(false, 'we are closed')
+const question = new Map([
+    ['Question', 'What is the best programming language in the world'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'Javascript'],
+    ['Correct', 3],
+    [true, 'Correct'],
+    [false, 'Try again']
 
-console.log(rest.get('name'))
-console.log(rest.get(true))
+])
 
-const time = 21;
+console.log(question)
 
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours))
+console.log(hoursMap)
 
-console.log(rest.has('categories'))
-rest.delete('2')
-console.log(rest)
+for (const [key, value] of question) {
+  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
+const answer = Number(prompt('Your answer'))
+console.log(answer)
 
-console.log(rest.size)
+console.log(question.get(question.get('correct') === answer))
 
-rest.set([1,2], 'Test')
-console.log(rest)
+//Convert map to array
 
-//not the same object as a key on rest
-console.log(rest.get([1,2]))
-
-//fix
-const arr = [1,2]
-rest.set(arr, 'Test')
-console.log(rest.get(arr))
-
-rest.set(document.querySelector('h1'), 'Heading')
-
-rest.clear()
-
+console.log([...question])
+console.log(question.keys())
+console.log(question.entries())
+console.log(question.values())

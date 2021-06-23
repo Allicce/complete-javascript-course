54c453691ef56553439c5301143fe307bf757f95
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-23 11:23:13 +0200

    strings part 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -591,6 +591,58 @@ checkMiddleSeat('11B')
 checkMiddleSeat('23C')
 checkMiddleSeat('3E')
 
+console.log(airline.toLocaleLowerCase())
+console.log(airline.toUpperCase())
+
+const passenger = 'jONas'
+const passengerLower = passenger.toLowerCase()
+const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect)
+
+//Comparing email
+const email = 'hello@jonas.io'
+const loginEmail = ' Hello@Jonas.Io \n'
+
+// const lowerEmail = loginEmail.toLowerCase()
+// const trimmedEmail = lowerEmail.trim()
+// console.log(trimmedEmail)
+
+const normalizedEmail = loginEmail.toLowerCase().trim()
+console.log(normalizedEmail)
+
+//replacing
+const priceGB = '288,97L'
+const priceUS = priceGB.replace('L', '$').replace(',','.')
+
+const announcement = 'All passengers come to boarding door 23, Boarding door 23'
+
+console.log(announcement.replace('door', 'gate'))
+// console.log(announcement.replaceAll('door', 'gate'))
+
+console.log(announcement.replace(/door/g, 'gate'))
+
+const plane = 'A32neo'
+
+console.log(plane.includes('A320'))
+console.log(plane.includes('Boeing'))
+console.log(plane.startsWith('Airb'))
+
+if(plane.startsWith('Airbus') && plane.endsWith('neo')) console.log('Part of the NEW Airbus family')
+
+const checkBaggage = (items) => {
+  const baggage = items.toLowerCase()
+  if(baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are not allowed on board')
+  } else {
+    console.log('Welcome aboard')
+  }
+}
+
+checkBaggage('I have a laptop, some Food and a pocket Knife')
+checkBaggage('Sock and camera')
+checkBaggage('Got some snacks and a gun for protection')
+
+
 
 
 

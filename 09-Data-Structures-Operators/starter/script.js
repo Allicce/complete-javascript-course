4c00e385d828f0dd816cec0f7ba00c1bda1ec5fa
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-23 08:38:06 +0200

    challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -479,34 +479,83 @@ GOOD LUCK 😀
 //
 // rest.clear()
 
-const question = new Map([
-    ['Question', 'What is the best programming language in the world'],
-    [1, 'C'],
-    [2, 'Java'],
-    [3, 'Javascript'],
-    ['Correct', 3],
-    [true, 'Correct'],
-    [false, 'Try again']
+// const question = new Map([
+//     ['Question', 'What is the best programming language in the world'],
+//     [1, 'C'],
+//     [2, 'Java'],
+//     [3, 'Javascript'],
+//     ['Correct', 3],
+//     [true, 'Correct'],
+//     [false, 'Try again']
+//
+// ])
+//
+// console.log(question)
+//
+// console.log(Object.entries(openingHours))
+// const hoursMap = new Map(Object.entries(openingHours))
+// console.log(hoursMap)
+//
+// for (const [key, value] of question) {
+//   if(typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+// }
+// const answer = Number(prompt('Your answer'))
+// console.log(answer)
+//
+// console.log(question.get(question.get('correct') === answer))
+//
+// //Convert map to array
+//
+// console.log([...question])
+// console.log(question.keys())
+// console.log(question.entries())
+// console.log(question.values())
 
-])
+///////////////////////////////////////
+// Coding Challenge #3
 
-console.log(question)
+/*
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
-console.log(Object.entries(openingHours))
-const hoursMap = new Map(Object.entries(openingHours))
-console.log(hoursMap)
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
 
-for (const [key, value] of question) {
-  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+console.log(gameEvents.values());
+const events = new Set(gameEvents.values());
+console.log(events);
+
+gameEvents.delete(64);
+
+
+const time = [...gameEvents.keys().pop()]
+console.log(time)
+console.log(`An event happend, on average, every ${time/ gameEvents.size} minutes`)
+
+for( const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`)
 }
-const answer = Number(prompt('Your answer'))
-console.log(answer)
 
-console.log(question.get(question.get('correct') === answer))
 
-//Convert map to array
 
-console.log([...question])
-console.log(question.keys())
-console.log(question.entries())
-console.log(question.values())
+

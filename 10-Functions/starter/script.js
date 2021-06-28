8f9a736d6a84a1a73306b2a919d83ad66a4feeef
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-28 10:51:54 +0200

    how passing arguments works: value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,20 +1,54 @@
 'use strict';
-const bookings = []
-//es6
-const createBooking = (flightNum, numPassengers = 1, price = 199 * numPassengers) => {
-    //before es6
-    // numPassengers = numPassengers || 1;
-    // price = price || 199
-    const booking = {
-        flightNum, numPassengers, price
-    }
-    console.log(booking)
-    bookings.push(booking)
+// const bookings = []
+// //es6
+// const createBooking = (flightNum, numPassengers = 1, price = 199 * numPassengers) => {
+//     //before es6
+//     // numPassengers = numPassengers || 1;
+//     // price = price || 199
+//     const booking = {
+//         flightNum, numPassengers, price
+//     }
+//     console.log(booking)
+//     bookings.push(booking)
+// }
+//
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 5);
+// createBooking('LH123', 6);
+//
+// createBooking('LH123', undefined, 1000);
+
+const flight = 'LH123';
+const jonas = {
+    name: 'Jonas Schedtmann',
+    passport: 12354866331
 }
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 5);
-createBooking('LH123', 6);
+const checkIn = (flightNum, passenger) => {
+    flightNum = 'LH999'
+    passenger.name = 'Mr.' + passenger.name
 
-createBooking('LH123', undefined, 1000);
+    if(passenger.passport === 12354866331) {
+        alert('Checked in')
+    } else {
+        alert('Wrong Passport')
+    }
+}
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// //name is rewrite to Mr. Jonas...
+// console.log(jonas);
+
+//create issue
+const newPassport = (person) => {
+    person.passport = Math.trunc(Math.random() * 100000000000)
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+//passing with reference is not existing in js only passing with value and in the value is saved reference to object
+
+

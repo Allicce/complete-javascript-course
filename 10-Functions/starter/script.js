33e54714e584b8886f3aefdb3853da164306648e
Alžbeta Štetiarová <alzbeta.stetiarova@moderntv.eu>
2021-06-28 10:36:28 +0200

    passing arguments: value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+const bookings = []
+//es6
+const createBooking = (flightNum, numPassengers = 1, price = 199 * numPassengers) => {
+    //before es6
+    // numPassengers = numPassengers || 1;
+    // price = price || 199
+    const booking = {
+        flightNum, numPassengers, price
+    }
+    console.log(booking)
+    bookings.push(booking)
+}
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+createBooking('LH123', 6);
+
+createBooking('LH123', undefined, 1000);

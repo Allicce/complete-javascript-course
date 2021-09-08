201904253eaf3c6291cf52dda8e07c441cf33708
allicce <stetiarova.alzbeta@gmail.com>
2021-09-08 21:24:38 +0200

    immediately invoked function expressions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -94,70 +94,70 @@
 // greeterHey('Adam')
 //
 // greet('Hello')('Jonas');
-
-const lufthansa = {
-    airline: 'Lufthansa',
-    iataCode: 'LH',
-    booking: [],
-    book(flightNum, name) {
-        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
-        this.booking.push({flight: `${this.iataCode}${flightNum}`, name})
-    }
-}
-
-lufthansa.book(239, 'Jonas Schedtmann')
-lufthansa.book(635, 'John Smith')
-
-const eurowings = {
-    airline: 'Eurowings',
-    iataCode: 'EW',
-    booking: [],
-}
-
-const book = lufthansa.book
-
-// 1. option to call method this on function - 1 parameter in call is where is bind this keywords and then we added
-// others parameters from originals function
-book.call(eurowings, 23, 'Sarah Wiliams')
-console.log(eurowings)
-
-book.call(lufthansa, 235, 'Mary Cooper')
-console.log(lufthansa)
-
-const swiss = {
-    airline: 'Swiss',
-    iataCode: 'LX',
-    booking: [],
-}
-
-book.call(swiss, 583, 'Marry Cooper')
-console.log(swiss)
-
-//2. apply method - is not used in modern javascript any more
-const flightData = [583, 'George Cooper']
-book.apply(swiss, flightData)
-console.log(swiss)
-
-//bind method - not call a book function - return new function
-const bookEW = book.bind(eurowings)
-const bookLH = book.bind(lufthansa)
-const bookLX = book.bind(swiss)
-
-bookEW(23, 'Steven Williams')
-console.log(eurowings)
-
-//we can preset other parameters from book function
-const bookEW23 = book.bind(eurowings, 23)
-bookEW23('Jonas Schedtmann')
-
-//with event listeners
-lufthansa.planes = 300
-lufthansa.buyPlane = function() {
-    console.log(this)
-
-    this.planes ++
-    console.log(this.planes)
-}
+//
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     booking: [],
+//     book(flightNum, name) {
+//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+//         this.booking.push({flight: `${this.iataCode}${flightNum}`, name})
+//     }
+// }
+//
+// lufthansa.book(239, 'Jonas Schedtmann')
+// lufthansa.book(635, 'John Smith')
+//
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     booking: [],
+// }
+//
+// const book = lufthansa.book
+//
+// // 1. option to call method this on function - 1 parameter in call is where is bind this keywords and then we added
+// // others parameters from originals function
+// book.call(eurowings, 23, 'Sarah Wiliams')
+// console.log(eurowings)
+//
+// book.call(lufthansa, 235, 'Mary Cooper')
+// console.log(lufthansa)
+//
+// const swiss = {
+//     airline: 'Swiss',
+//     iataCode: 'LX',
+//     booking: [],
+// }
+//
+// book.call(swiss, 583, 'Marry Cooper')
+// console.log(swiss)
+//
+// //2. apply method - is not used in modern javascript any more
+// const flightData = [583, 'George Cooper']
+// book.apply(swiss, flightData)
+// console.log(swiss)
+//
+// //bind method - not call a book function - return new function
+// const bookEW = book.bind(eurowings)
+// const bookLH = book.bind(lufthansa)
+// const bookLX = book.bind(swiss)
+//
+// bookEW(23, 'Steven Williams')
+// console.log(eurowings)
+//
+// //we can preset other parameters from book function
+// const bookEW23 = book.bind(eurowings, 23)
+// bookEW23('Jonas Schedtmann')
+//
+// //with event listeners
+// lufthansa.planes = 300
+// lufthansa.buyPlane = function() {
+//     console.log(this)
+//
+//     this.planes ++
+//     console.log(this.planes)
+// }
 
 //this keyword is point to button element
 // document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane)
@@ -215,36 +215,47 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-const poll = {
-    question: 'What is your favourite programming language?',
-    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-    // This generates [0, 0, 0, 0]. More in the next section 😃
-    answers: new Array(4).fill(0),
-    registerNewAnswer() {
-        const answer = Number (prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`))
+// const poll = {
+//     question: 'What is your favourite programming language?',
+//     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//     // This generates [0, 0, 0, 0]. More in the next section 😃
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer() {
+//         const answer = Number (prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`))
+//
+//         typeof answer === 'number' &&
+//         answer < this.answers.length &&
+//         this.answers[answer]++
+//
+//         this.displayResults()
+//         this.displayResults('string')
+//     },
+//     displayResults(type = 'array') {
+//         if(type === 'array') {
+//             console.log(this.answers)
+//         } else if (type === 'string') {
+//             console.log(`Poll results are ${this.answers.join(', ')}`)
+//         }
+//     }
+// }
+//
+// document
+//     .querySelector('.poll')
+//     .addEventListener('click', poll.registerNewAnswer.bind(poll));
+//
+// //poll.registerNewAnswer()
 
-        typeof answer === 'number' &&
-        answer < this.answers.length &&
-        this.answers[answer]++
+// // [5, 2, 3]
+//
+// poll.displayResults.call({answers: [5, 2, 3]}, 'string')
 
-        this.displayResults()
-        this.displayResults('string')
-    },
-    displayResults(type = 'array') {
-        if(type === 'array') {
-            console.log(this.answers)
-        } else if (type === 'string') {
-            console.log(`Poll results are ${this.answers.join(', ')}`)
-        }
-    }
-}
+//IIFE
+(function() {
+    console.log('this will never run again')
+    const isPrivate = 23;
+})();
 
-document
-    .querySelector('.poll')
-    .addEventListener('click', poll.registerNewAnswer.bind(poll));
+// console.log(isPrivate)
 
-//poll.registerNewAnswer()
 
-// [5, 2, 3]
-
-poll.displayResults.call({answers: [5, 2, 3]}, 'string')
+(() => console.log('this will never run again'))()

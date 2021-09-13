f505348725b2539de8c56d179f448a3017b291d9
allicce <stetiarova.alzbeta@gmail.com>
2021-09-13 21:19:56 +0200

    coding challenge 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -278,55 +278,77 @@ GOOD LUCK 😀
 // console.dir(booker)
 
 
-//example 1
-let f;
-const g = function () {
-    const a = 23;
-    f = function() {
-        console.log(a * 2);
-    }
-}
+// //example 1
+// let f;
+// const g = function () {
+//     const a = 23;
+//     f = function() {
+//         console.log(a * 2);
+//     }
+// }
+//
+// const h = function () {
+//     const b = 777;
+//     f = function() {
+//         console.log(b * 2);
+//     }
+// }
+//
+// g();
+// f();
+// //re-assigning the f function
+// h();
+// f();
+//
+// console.dir(f);
+//
+// //example 2
+//
+// const boardPassengers = function (n, wait) {
+//     const perGroup = n / 3;
+//
+//     setTimeout(function () {
+//         console.log(`we are now boarding all ${n} passengers`);
+//         console.log(`there are three groups, each with ${perGroup} passengers`)
+//     }, wait * 1000)
+//
+//     console.log(`we start boarding in ${wait} seconds`);
+// }
+//
+// boardPassengers(180, 3);
+//
+// const boardPassengers2 = function (n, wait) {
+//
+//     setTimeout(function () {
+//         console.log(`we are now boarding all ${n} passengers`);
+//         console.log(`there are three groups, each with ${perGroup} passengers`)
+//     }, wait * 1000)
+//
+//     console.log(`we start boarding in ${wait} seconds`);
+// }
+//
+// const perGroup = 1800;
+//
+// boardPassengers2(180, 3);
 
-const h = function () {
-    const b = 777;
-    f = function() {
-        console.log(b * 2);
-    }
-}
+//////////////////////////////////////
+// Coding Challenge #2
 
-g();
-f();
-//re-assigning the f function
-h();
-f();
+/*
+This is more of a thinking challenge than a coding challenge 🤓
 
-console.dir(f);
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
 
-//example 2
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
 
-const boardPassengers = function (n, wait) {
-    const perGroup = n / 3;
+GOOD LUCK 😀
+*/
 
-    setTimeout(function () {
-        console.log(`we are now boarding all ${n} passengers`);
-        console.log(`there are three groups, each with ${perGroup} passengers`)
-    }, wait * 1000)
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
 
-    console.log(`we start boarding in ${wait} seconds`);
-}
-
-boardPassengers(180, 3);
-
-const boardPassengers2 = function (n, wait) {
-
-    setTimeout(function () {
-        console.log(`we are now boarding all ${n} passengers`);
-        console.log(`there are three groups, each with ${perGroup} passengers`)
-    }, wait * 1000)
-
-    console.log(`we start boarding in ${wait} seconds`);
-}
-
-const perGroup = 1800;
-
-boardPassengers2(180, 3);
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
+})();
